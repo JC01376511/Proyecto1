50639cc541e44924a8c5d237ed93c81877194dbc
Juan Carlos <A01376511@tec.mx>
2024-05-06 14:22:55 -0600

    Create Actividad 2.R

---
+++ retoAnalitica/Codigo/Actividad 2.R
@@ -0,0 +1,17 @@
+# Ejercicio 2 -------------------------------------------------------------
+lado <- 5
+area <- lado * lado
+print(area)
+
+# Ejercicio 4 -------------------------------------------------------------
+radio <- 100
+pi <- 3.1416
+area <- pi * (radio ^ 2)
+print(area)
+
+# Ejercicio 6 -------------------------------------------------------------
+cantidadPesos <- 250
+dolares <- 20
+cambio <- dolares * cantidadPesos
+print(cambio)
+
