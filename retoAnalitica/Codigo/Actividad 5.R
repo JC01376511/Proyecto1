161a875f52a29621ff9759b7cc23ff5476a5c951
Juan Carlos <A01376511@tec.mx>
2024-05-08 15:36:05 -0600

    Update Actividad 5.R

--- retoAnalitica/Codigo/Actividad 5.R
+++ retoAnalitica/Codigo/Actividad 5.R
@@ -1,11 +1,25 @@
 #Actividad 5
+install.packages("factoextra")
+library(factoextra)
+library(ggplot2)
 
 #Cargar dataset en R
 data("mtcars")
 View(mtcars)
 
-mtcars$mpg = NULL
 kM <- kmeans(mtcars,3)
 kM
 
+#Graficación de clusters
+fviz_cluster(kM,mtcars)
+
+#Escalando datos
+mtcarsEscalado <- scale(as.matrix(mtcars[, 1:4]))
+kM1 <- kmeans(mtcarsEscalado,4)
+fviz_cluster(kM1,mtcarsEscalado)
+
+#Gráfica matriz de correlación
+library(corrplot)
+corrCar <- cor(mtcars)
+corrplot(corrCar,method = "ellipse")
 
