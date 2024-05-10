e7d19d6ecbc96545ec4e691213f3d524b4a5a6b0
JC01376511 <169090133+JC01376511@users.noreply.github.com>
2024-05-09 20:48:14 -0600

    Update Actividad 2.R

--- retoAnalitica/Codigo/Actividad 2.R
+++ retoAnalitica/Codigo/Actividad 2.R
@@ -1,6 +1,19 @@
+#Actividad 2 Juan Carlos Flores García A01376511, Daniela Román Cruz A01799267
+#Ejercicio 1 --------------------------------------------------------------
+numero1 <- 5 
+numero2 <- 7 
+suma <- numero1 + numero2
+print(suma)
+
 # Ejercicio 2 -------------------------------------------------------------
 lado <- 5
 area <- lado * lado
+print(area)
+
+#Ejercicio 3 --------------------------------------------------------------
+base <- 18
+altura <- 9
+area <- (base*altura)/2
 print(area)
 
 # Ejercicio 4 -------------------------------------------------------------
@@ -9,9 +22,19 @@ pi <- 3.1416
 area <- pi * (radio ^ 2)
 print(area)
 
+# Ejercicio 5 -------------------------------------------------------------
+radio <- 8
+altura <- 16
+volumen <- (1/3)*pi*radio^2*altura
+print(volumen)
+
 # Ejercicio 6 -------------------------------------------------------------
 cantidadPesos <- 250
 dolares <- 20
 cambio <- dolares * cantidadPesos
 print(cambio)
 
+# Ejercicio 7 -------------------------------------------------------------
+Temp_F <- 105
+Temp_C <- (5/9)*(Temp_F - 32)
+print(paste("La temperatura en Celsius es:", format(Temp_C, nsmall=1)))
